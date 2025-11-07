510b0f955b9d24c6878d781abac8b18ebd3d985f
Esther van Driessche <esther.van.driessche@student.howest.be>
2025-11-08 01:11:50 +0900

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://esther-vd.github.io/',
+    base: '/portfolio',
+});
