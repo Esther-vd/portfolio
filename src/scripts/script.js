a2ab4101eb2a999865acaa512e11fe212914aac7
Esther van Driessche <esther.van.driessche@student.howest.be>
2025-11-14 22:17:33 +0900

    animated header icon folder

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -20,8 +20,95 @@ const hamburger = () => {
   });
 };
 
+//gsap
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import TextPlugin from "gsap/TextPlugin";
+import DrawSVGPlugin from "gsap/DrawSVGPlugin";
+
+const gsapCode = () => {
+  gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, TextPlugin);
+  gsapIndex();
+  folderGsap();
+};
+
+const gsapIndex = () => {
+  ScribblesGsap();
+};
+
+const ScribblesGsap = () => {
+  let scribleHeroTl = gsap.timeline({
+    paused: false,
+  });
+  scribleHeroTl
+    .from(".scribble__arrow--hero--line", { duration: 0.3, drawSVG: 0 })
+    .from(".scribble__arrow--hero--arrow", { duration: 0.2, drawSVG: 0 });
+
+  let scribleTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".scribble__arrow--plantin--line",
+      start: "top 80%",
+      end: "top 20%",
+      toggleActions: "play none reverse none",
+      scrub: 0,
+    },
+  });
+  scribleTl
+    .from(".scribble__arrow--plantin--line", { duration: 0.3, drawSVG: 0 })
+    .from(".scribble__arrow--plantin--arrow", { duration: 0.2, drawSVG: 0 });
+};
+
+const folderGsap = () => {
+  document
+    .querySelectorAll(".folder__svg")
+    .forEach((element) =>
+      element.addEventListener("mouseenter", () => folderHandler(element))
+    );
+
+  document
+    .querySelectorAll(".folder__svg")
+    .forEach((element) =>
+      element.addEventListener("mouseleave", () =>
+        folderReverseHandler(element)
+      )
+    );
+};
+
+const folderHandler = (element) => {
+  let folderAnimation = gsap.timeline({});
+
+  folderAnimation
+    .to(element, {
+      rotation: 5,
+      scale: 0.95,
+      duration: 0.3,
+    })
+    .to(element, {
+      rotation: -3,
+      duration: 0.4,
+    });
+};
+
+const folderReverseHandler = (element) => {
+  let folderAnimation = gsap.timeline({});
+  
+  folderAnimation
+    .to(element, {
+      rotation: 3,
+      scale: 1,
+      duration: 0.4,
+    })
+    .to(element, {
+      rotation: 0,
+      duration: 0.3,
+    });
+};
+
+
 // click event
 const headerClickEvent = () => {
+let headerIconTl =  gsap.timeline({
+})
   if (document.querySelector(".header__icon__folder")) {
     document
       .querySelector(".header__icon__folder")
@@ -29,8 +116,14 @@ const headerClickEvent = () => {
         const height = document
           .querySelector(".header__icon__folder")
           .getBoundingClientRect().height;
-        document.querySelector(".header__icon__folder").style.display = "none";
-        document.querySelector(".header__icon__file").style.display = "block";
+        headerIconTl
+          .to(".header__icon__folder .folder__svg", { y: 20, duration: 0.2 })
+          .to(".header__icon__folder", { opacity: 0, duration: 0 })
+          .to(".header__icon__file", { opacity: 1, duration: 0 })
+          .from(".header__icon__file", { scale: .9, duration: 0.2 })
+          .to(".header__icon__file", { y: -10, duration: 0.2 }, "<")
+          .to(".header__icon__file", { y: 10, duration: 0.2 });
+
         document.querySelector(
           ".header__icon__file"
         ).style.height = `${height}px`;
@@ -38,39 +131,6 @@ const headerClickEvent = () => {
   }
 };
 
-//gsap 
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import TextPlugin from "gsap/TextPlugin";
-import DrawSVGPlugin from "gsap/DrawSVGPlugin";
-
-const gsapCode = () => {
-     gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, TextPlugin);
-     gsapIndex();
-}
-
-const gsapIndex = () => {
-    let scribleHeroTl = gsap.timeline({
-      paused: false,
-    });
-    scribleHeroTl
-      .from(".scribble__arrow--hero--line", { duration: 0.3, drawSVG: 0 })
-      .from(".scribble__arrow--hero--arrow", { duration: 0.2, drawSVG: 0 });
-    
-      let scribleTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".scribble__arrow--plantin--line",
-        start: "top 80%",
-        end: "top 20%",
-        toggleActions: "play none reverse none",
-        scrub: 0,
-        markers: true,
-      },
-    });
-    scribleTl
-      .from(".scribble__arrow--plantin--line", { duration: 0.3, drawSVG: 0 })
-      .from(".scribble__arrow--plantin--arrow", { duration: 0.2, drawSVG: 0 });
-}
 
 const init = () => {
   window.addEventListener("resize", updateScaleFactor);
